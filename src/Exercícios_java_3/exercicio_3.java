f2581382c09de418b66ac57e2010daa5a0e395c5
Gabryel Silva <gabryelssantos@gmail.com>
2022-09-03 15:19:28 -0300

    atualizando

--- src/Exercícios_java_3/exercicio_3.java
+++ src/Exercícios_java_3/exercicio_3.java
@@ -6,17 +6,21 @@ public class exercicio_3 {
 	public static void main(String[] args) {
 		Scanner s;
 		s = new Scanner (System.in);
+		
 		//3. Leia dois números diferentes e informe qual deles é o maior.
 		
 		int a, b;
+		
 		System.out.println("Digite o valor de A");
 		a = s.nextInt();
+		
 		System.out.println("Digite o valor de B");
 		b = s.nextInt();
+		
 		if(a > b) {
-		System.out.println("A é maior que B");
+			System.out.println("A é maior que B");
 		}else {
-		System.out.println("B é maior que A");
+			System.out.println("B é maior que A");
 		}
 
 	}
